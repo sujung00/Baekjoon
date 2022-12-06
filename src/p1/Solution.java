7f4cdbac150a82484d23a02469f8d60bb7dd3882
sujung00 <rlatnwjdirin@naver.com>
2022-12-06 21:15:51 +0900

    옹알이(1), 다음에 올 숫자

---
+++ src/p1/Solution.java
@@ -0,0 +1,22 @@
+package p1;
+
+import java.util.Scanner;
+
+public class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String[] say = {"aya", "ye", "woo", "ma"};
+
+        for (int i = 0; i < babbling.length; i++){
+            int count = babbling[i].length();
+            for (int j = 0; j < say.length; j++){
+                if (babbling[i].contains(say[j])) {
+                    count -= say[j].length();
+                }
+            }
+            if (count == 0) answer++;
+        }
+
+        return answer;
+    }
+}
--- src/test2/Main.java
+++
@@ -1,9 +0,0 @@
-package test2;
-
-public class Main {
-    public static void main(String[] args) {
-        for (int i = 0; i < 2; i++){
-            System.out.println("강한친구 대한육군");
-        }
-    }
-}
