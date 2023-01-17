d0f952adca40959c11c5ed02d8385b6ee0e24d61
sujung00 <rlatnwjdirin@naver.com>
2023-01-17 20:01:12 +0900

    대문자와 소문자

---
+++ src/Lv0/p24/Solution.java
@@ -0,0 +1,18 @@
+package Lv0.p24;
+
+// 대문자와 소문자
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+
+        for (int i = 0; i < my_string.length(); i++){
+            if (Character.isUpperCase(my_string.charAt(i))){
+                answer += Character.toLowerCase(my_string.charAt(i));
+            } else {
+                answer += Character.toUpperCase(my_string.charAt(i));
+            }
+        }
+
+        return answer;
+    }
+}
