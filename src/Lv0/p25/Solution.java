7b930bafa3343a63f4114dd3814f8cbb01ce5e49
sujung00 <rlatnwjdirin@naver.com>
2023-01-18 22:09:43 +0900

    암호 해독

---
+++ src/Lv0/p25/Solution.java
@@ -0,0 +1,14 @@
+package Lv0.p25;
+
+// 암호 해독
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+
+        for (int i = code-1; i < cipher.length(); i+=code) {
+            answer += cipher.charAt(i);
+        }
+
+        return answer;
+    }
+}
