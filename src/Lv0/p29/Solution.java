7034acd0f1c825c9b978c46e19642a2cea294177
sujung00 <rlatnwjdirin@naver.com>
2023-01-23 09:40:15 +0900

    중복된 문자 제거(다른 풀이)

--- src/Lv0/p29/Solution.java
+++ src/Lv0/p29/Solution.java
@@ -1,18 +1,27 @@
 package Lv0.p29;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 // 중복된 문자 제거*
+//class Solution {
+//    public String solution(String my_string) {
+//        char[] ch = my_string.toCharArray();
+//        List<String> arr = new ArrayList();
+//        for (int i = 0; i < my_string.length(); i++) {
+//            if (!arr.contains(String.valueOf(ch[i]))) {
+//                arr.add(String.valueOf(ch[i]));
+//            }
+//        }
+//        return String.join("", arr);
+//    }
+//}
+
+// 다른 풀이
 class Solution {
     public String solution(String my_string) {
-        char[] ch = my_string.toCharArray();
-        List<String> arr = new ArrayList();
-        for (int i = 0; i < my_string.length(); i++) {
-            if (!arr.contains(String.valueOf(ch[i]))) {
-                arr.add(String.valueOf(ch[i]));
-            }
-        }
-        return String.join("", arr);
+        String[] answer = my_string.split("");
+        Set<String> set = new LinkedHashSet<String>(Arrays.asList(answer));
+
+        return String.join("", set);
     }
 }
