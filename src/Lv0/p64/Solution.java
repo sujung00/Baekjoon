cfc4f7075d0bc19e85140422cbcca718ec8af090
sujung00 <rlatnwjdirin@naver.com>
2023-02-26 22:35:28 +0900

    구슬을 나누는 경우의 수

---
+++ src/Lv0/p64/Solution.java
@@ -0,0 +1,20 @@
+package Lv0.p64;
+
+// 구슬을 나누는 경우의 수
+class Solution {
+    public int combination(int n, int m){
+        if (m == 0 || n == m){
+            return 1;
+        } else {
+            return combination(n-1, m-1) + combination(n-1, m);
+        }
+    }
+
+    public int solution(int balls, int share) {
+        int answer = 0;
+
+        answer = combination(balls, share);
+
+        return answer;
+    }
+}
